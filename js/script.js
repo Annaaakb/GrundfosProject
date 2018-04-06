adf47767e80582d563f466592a87abb80a596f90
trinelangberg <trinelangberg@icloud.com>
2018-04-06 12:30:09 +0200

    minus topics

--- js/script.js
+++ js/script.js
@@ -130,14 +130,3 @@ for (var i = 0; i < btns.length; i++) {
   });
 }
 
-
-
-// Range slider
-
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-output.innerHTML = slider.value;
-
-slider.oninput = function() {
-  output.innerHTML = this.value;
-}
