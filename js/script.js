5ef1c433b606ed8d7dbe0c91564980b82c4ff7af
Annaaakb <36158732+Annaaakb@users.noreply.github.com>
2018-04-10 09:56:53 +0200

    Rettelser class/id name

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@
 
 
 function sliderClickRight1(){
-	$('#hi .aaa').addClass('XXX').removeClass('aaa');
-	$('#hi .bbb').addClass('aaa').removeClass('bbb');
-	$('#hi .XXX:nth-child(3)').addClass('bbb').removeClass('XXX');
-	$('#hi ul.slider_main').append($('#hi li.XXX:nth-child(1)'));
+	$('#Jumpbox .aaa').addClass('XXX').removeClass('aaa');
+	$('#Jumpbox .bbb').addClass('aaa').removeClass('bbb');
+	$('#Jumpbox .XXX:nth-child(3)').addClass('bbb').removeClass('XXX');
+	$('#Jumpbox ul.slider_main').append($('#Jumpbox li.XXX:nth-child(1)'));
 }
 function sliderClickLeft1(){
-	$('#hi .bbb').addClass('XXX').removeClass('bbb');
-	$('#hi .aaa').addClass('bbb').removeClass('aaa');
-	$('#hi ul.slider_main').prepend($('#hi li.XXX:last-child'));
-	$('#hi .XXX:nth-child(1)').addClass('aaa').removeClass('XXX');
+	$('#Jumpbox .bbb').addClass('XXX').removeClass('bbb');
+	$('#Jumpbox .aaa').addClass('bbb').removeClass('aaa');
+	$('#Jumpbox ul.slider_main').prepend($('#Jumpbox li.XXX:last-child'));
+	$('#Jumpbox .XXX:nth-child(1)').addClass('aaa').removeClass('XXX');
 }
 
 
